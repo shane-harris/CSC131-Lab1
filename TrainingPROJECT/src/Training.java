a1a6cc04bd9e04dd2343df28aa5978519be73b95
rockb <rockb@LAPTOP-LJQVDAR6>
2018-10-26 10:43:41 -0700

    Shane's Submission

---
+++ TrainingPROJECT/src/Training.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+public class Training {
+	/* This is a training class I created for out team so that we can
+	 * get familiar with Git and all know how to push and pull and commit
+	 * work.
+	 * 
+	 * For now everyone just add a System.out statement with your name
+	 * and commit and push it to the repo. This will all be on the 
+	 * master branch because this project if just for our practice*/
+	public static void main(String[] args){
+	
+	System.out.println("Shane Harris");
+	}
+}
