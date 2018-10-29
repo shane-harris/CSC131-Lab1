edfd8e343842fedc2f3c5255fa25aa9f29ab64da
rockb <rockb@10.117.105.144>
2018-10-29 16:50:18 -0700

    Added comment and for loop for merge conflict practice

--- TrainingPROJECT/src/Training.java
+++ TrainingPROJECT/src/Training.java
@@ -15,5 +15,22 @@ public class Training {
 		System.out.println("Connor Gaymon");
 		System.out.println("Joseph Crouson");
 		System.out.println("Jake Reynolds");
+		
+		/*The next thing that we will practice is creating a merge conflict
+		 * and how to resolve the conflict. This will require two people
+		 * each time we practice. I will volunteer to be everyones partner
+		 * 
+		 * A Merge conflict arises when two or more programmers pull from 
+		 * the repo. make different changes and both try to commit. the
+		 * first to commit will see no issue but the second will need to 
+		 * resolve the conflict.
+		 * 
+		 * I will create a for loop and then the next user will change the
+		 * loop to a while loop or do while or whatever you want as long as
+		 * its not the same code.
+		 * */
+		for(int i=0; i<5;i++) {
+			System.out.format("Out is %i",i);
+		}
 	}
 }
