ee0938e966e3e2e25dd9becdbf059e620791f66f
rockb <rockb@10.0.0.113>
2018-10-31 23:13:21 -0700

    second try

--- TrainingPROJECT/src/Training.java
+++ TrainingPROJECT/src/Training.java
@@ -17,7 +17,7 @@ public class Training {
 		System.out.println("Jake Reynolds");
 		System.out.println("Ian Charamuga");
 		System.out.println("Everyone finished");
-		/*testing removal of branch*/
+		/*testing removal of branch1*/
 		
 		/*The next thing that we will practice is creating a merge conflict
 		 * and how to resolve the conflict. This will require two people
